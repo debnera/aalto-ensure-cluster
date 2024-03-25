458a61750d0d10398075e748f97c9977036df0f6
wickstjo <jfwick@gmail.com>
2024-03-26 00:46:05 +0200

    Wrote the first draft of the yolo docs

---
+++ 04_yolo/init_and_deploy.sh
@@ -0,0 +1,15 @@
+# MAKE SERVICE DIRECTORY ARGUMENT MANDATORY
+if [[ -z $1 ]]
+then
+  echo "ERROR: DEFINE THE NUMBER OF TOPIC PARTITIONS";
+  exit 1;
+fi
+
+# INITIALIZE KAFKA TOPICS & WAIT FOR ABIT
+python3 app/kafka_init.py -n $1
+sleep 5
+
+read -p "KAFKA TOPICS READY, PRESS ENTER TO DELOY PODS..."
+
+# THEN DEPLOY FRESH PODS
+kubectl apply -f yolo_depl.yaml
