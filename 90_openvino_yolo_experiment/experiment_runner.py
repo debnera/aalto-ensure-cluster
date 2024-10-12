f12f86e3010c0ba7932e3630a4017cc590ec4206
Anton Debner <anton.debner@aalto.fi>
2024-10-12 08:54:44 +0300

    Copy more information about the run to the zip (git commit and experiment_runner.py)

--- 90_openvino_yolo_experiment/experiment_runner.py
+++ 90_openvino_yolo_experiment/experiment_runner.py
@@ -95,6 +95,19 @@ def zip_snapshot(snapshot_path, yolo_csv_folder=None, name="yolov8n"):
         shutil.copytree(yolo_csv_folder, destination_yolo_folder)
         log(f"Copied YOLO CSV files from {yolo_csv_folder} to {destination_yolo_folder}")
 
+    # Copying the current script and its configurations to the snapshot
+    current_script_path = __file__  # Path to the current script
+    script_destination = os.path.join(snapshot_path, os.path.basename(current_script_path))
+    shutil.copy2(current_script_path, script_destination)
+    log(f"Copied current script to {script_destination}")
+    # Extracting git commit and date
+    result = subprocess.run(["git", "log", "-1", "--pretty=format:%H %ad"], capture_output=True, text=True)
+    git_commit_info = result.stdout.strip()
+    git_info_path = os.path.join(snapshot_path, "git_commit_info.txt")
+    with open(git_info_path, "w") as git_info_file:
+        git_info_file.write(git_commit_info)
+    log(f"Copied git commit info to {git_info_path}")
+
     # Create a zip file from the snapshot_path
     zip_filename = f"{int(start_zip_time)}_{name}.zip"
     zip_filepath = os.path.join(os.path.dirname(snapshot_path), zip_filename)
